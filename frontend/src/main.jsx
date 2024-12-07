dbf81a59641a66220aa3fabf88d6ed8309ccc484
Amos <amos7perez@gmail.com>
2024-12-07 12:19:21 -0500

    add Auth0 React SDK

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
